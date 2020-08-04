024e386747e1b873039474ea339a0fdd8ef570e1
ronygigi <ronygigi@gmail.com>
2020-08-04 18:21:17 +0530

    Added association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_063748) do
+ActiveRecord::Schema.define(version: 2020_08_04_065445) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
